e94cdd8ebc9e0daf9f8a5bc8f31ef1ef5e6eb64e
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-03-28 14:06:37 +0200

    created a setup script for easy dotfiles install on site
    Usage:
    
        setup.sh [--no-backup] [--copy|--symlink] [--srcdir=DIR] [--destdir=DIR]
    
    Copy (or symlink) dotfiles from a specified source directory (defaults
    to the script directory), to a specified destination directory (defaults
    to user's $HOME).
    Before setting dot files in place, backup all user files that would be
    overwritten (by default; optionally, backup can be turned off). The
    backup is in ``<repo dir>/backup/<iso datetime>``.
    
    The setup asks for confirmation before starting.

---
+++ setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Defaults: symlink dotfiles from the directory that contains `setup.sh`
+# to user's home, backup here under `backup` dir.
+srcdir=$(dirname "$0")
+destdir="$HOME"
+backup=1
+method=symlink
+
+while (( $# )); do
+    case "$1" in
+        --srcdir=*)
+            srcdir="${1##--srcdir=}"
+            shift;;
+        --destdir=*)
+            destdir="${1##--destdir=}"
+            shift;;
+        --no-backup)
+            backup=0
+            shift;;
+        --copy)
+            method=copy
+            shift;;
+        --symlink)
+            method=symlink
+            shift;;
+        *)
+            echo "Unrecognized param '$1'."
+            echo "Usage: setup.sh [--no-backup] [--copy|--symlink] [--srcdir=DIR] [--destdir=DIR]"
+            exit 255
+            ;;
+    esac
+done
+
+if [ ! -d "$srcdir" ]; then
+    echo "Source directory not found: $srcdir"
+    exit 1
+else
+    srcdir=$(readlink -f "$srcdir")
+fi
+
+if [ ! -d "$destdir" ]; then
+    echo "Destination directory not found: $destdir"
+    exit 1
+else
+    destdir=$(readlink -f "$destdir")
+fi
+
+(( backup )) && backup_desc="yes" || backup_desc="no"
+backupdir="$srcdir/backup/$(date -Is)"
+
+echo "Installing dotfiles:"
+echo " - from: $srcdir"
+echo " - to: $destdir"
+echo " - method: $method"
+echo " - backup original files? $backup_desc"
+(( backup )) && echo " - backup location: $backupdir"
+echo
+read -p "Proceed [Y/n]? " ans
+case "${ans:-y}" in
+    Y|y);;
+    *) exit 2
+esac
+
+(( backup )) && { mkdir -p "$backupdir" || exit 3; }
+for dotfile in $(find . -maxdepth 1 -type f -name '.*'); do
+    echo "Processing: $dotfile"
+    filename=$(basename "$dotfile")
+    usrfile="$destdir/$filename"
+    if (( backup )) && [ -f "$usrfile" ]; then
+        cp -a "$usrfile" "$backupdir"
+    fi
+    if [ "$method" == "copy" ]; then
+        cp -af "$dotfile" "$destdir"
+    elif [ "$method" == "symlink" ]; then
+        ln -sf -t "$destdir" $(readlink -f "$dotfile")
+    fi
+done
+echo "Done."
